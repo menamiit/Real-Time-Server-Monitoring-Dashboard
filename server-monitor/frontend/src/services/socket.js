f5a2cd2bb4393bceff33c9066d19f556b5372402
Namit <pareeknamit8@gmail.com>
2025-12-02 18:43:30 +0530

    Added services

---
+++ server-monitor/frontend/src/services/socket.js
@@ -0,0 +1,66 @@
+import { io } from 'socket.io-client';
+
+const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || 'http://localhost:5000';
+
+class SocketService {
+  constructor() {
+    this.socket = null;
+  }
+
+  connect() {
+    if (!this.socket) {
+      this.socket = io(SOCKET_URL, {
+        transports: ['websocket'],
+        autoConnect: true
+      });
+
+      this.socket.on('connect', () => {
+        console.log('Socket connected:', this.socket.id);
+      });
+
+      this.socket.on('disconnect', () => {
+        console.log('Socket disconnected');
+      });
+
+      this.socket.on('connect_error', (error) => {
+        console.error('Socket connection error:', error);
+      });
+    }
+    return this.socket;
+  }
+
+  disconnect() {
+    if (this.socket) {
+      this.socket.disconnect();
+      this.socket = null;
+    }
+  }
+
+  on(event, callback) {
+    if (this.socket) {
+      this.socket.on(event, callback);
+    }
+  }
+
+  off(event, callback) {
+    if (this.socket) {
+      this.socket.off(event, callback);
+    }
+  }
+
+  emit(event, data) {
+    if (this.socket) {
+      this.socket.emit(event, data);
+    }
+  }
+
+  subscribe(serverId) {
+    this.emit('subscribe', serverId);
+  }
+
+  unsubscribe(serverId) {
+    this.emit('unsubscribe', serverId);
+  }
+}
+
+export default new SocketService();
